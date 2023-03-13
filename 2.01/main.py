bcdca03a1f3567b4a2718a99a96ab6efe85abbd2
KiraUmea <flammeten@gmail.com>
2023-03-13 16:36:53 +0200

    h

---
+++ 2.01/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+a = int(input())
+b = a % 2
+if b == 1:
+  print("Нечетное")
+elif b == 0:
+  print("Четное")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
